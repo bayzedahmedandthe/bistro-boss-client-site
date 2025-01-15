32851972a04fdd2d1c6b6d4a04439a14120e62ba
bayzedahmedandthe <bayzedm784@.com>
2025-01-15 09:03:52 +0600

    Use react-hook-form on AddItem page

--- src/Components/AddItems.jsx
+++ src/Components/AddItems.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import SectionTitle from "../Shared/SectionTitle/SectionTitle"
+import { useForm } from "react-hook-form"
+import { FaUtensils } from 'react-icons/fa';
+
 
 const AddItems = () => {
+    const { register, handleSubmit } = useForm()
+    const onSubmit = (data) => console.log(data)
     return (
         <div>
-            AddItems page here
+            <SectionTitle heading="Add An Item" subHeading="What's new?"></SectionTitle>
+            <div className='bg-white w-4/5 mx-auto mb-20'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label className="form-control w-full md:px-8 md:py-4">
+                        <span className="label-text py-2 text-lg">Recipe name*</span>
+                        <input {...register("name", {required: true})} type="text" placeholder="recipe name" className="input input-bordered w-full bg-gray-100" />
+                    </label>
+                    <div className='md:flex md:items-center md:justify-between '>
+                        <label className="form-control w-full md:px-8 md:py-4">
+                            <span className="label-text py-2 text-lg">Select a category*</span>
+                            <select {...register("category", {required: true})} className="select select-bordered w-full bg-gray-100">
+                                <option disabled selected>Category</option>
+                                <option>Salad</option>
+                                <option>Pizza</option>
+                                <option>Soup</option>
+                                <option>Dessert</option>
+                                <option>Drinks</option>
+                            </select>
+                        </label>
+                        <label className='w-full form-control md:px-8 md:py-4'>
+                            <span className="label-text py-2 text-lg">Price*</span>
+                            <input {...register("price", {required: true})} type="text" placeholder="price" className="input input-bordered w-full bg-gray-100" />
+                        </label>
+                    </div>
+                    <label className="form-control w-full md:px-8 md:py-4">
+                        <span className="label-text py-2 text-lg">Recipe Details</span>
+                        <textarea {...register("recipe", {required: true})} className="textarea textarea-bordered h-28 bg-gray-100 " placeholder="Recipe Details"></textarea>
+                    </label>
+                    <div className=' md:px-8 md:py-4'>
+                        <input {...register("image", {required: true})} type="file" className="file-input bg-gray-100 w-full max-w-xs" />
+                    </div>
+
+                    <div className="md:px-8 md:py-4">
+                        <button className='bg-[#D1A054] py-2 px-4 text-lg btn'>
+                            Add Item <FaUtensils></FaUtensils>
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -1,20 +1,18 @@
 import { Navigate, useLocation } from "react-router-dom";
-import useAdmin from "../Hooks/useAdmin";
 import useAuth from "../Hooks/useAuth";
-
+import useAdmin from "../Hooks/useAdmin";
 
 const AdminRoute = ({children}) => {
+    const {user, loading} = useAuth();
     const [isAdmin, isAdminLoading] = useAdmin();
-    const { user, loading } = useAuth();
     const location = useLocation();
-    if (loading || isAdminLoading) {
+    if(loading || isAdminLoading){
         return <div className="min-h-screen flex justify-center"><span className="loading loading-ball loading-lg"></span></div>
     }
-    if (user || isAdmin) {
-        return children
+    if(user && isAdmin){
+        return children;
     }
+    return <Navigate to="/login" state={{from: location}}></Navigate>
 
-    return <Navigate to="/login" state={{ from: location }} replace></Navigate>
 };
-
 export default AdminRoute;
