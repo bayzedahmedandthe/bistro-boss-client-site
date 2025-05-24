d4224a84701c4ca052409a991d8cdeca968c6bf0
bayzedahmedandthe <bayzedm784@.com>
2025-05-24 18:23:21 +0600

    Added   withCredentials

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: "https://bistro-boss-server-e872f67a2-bayzed-ahmeds-projects.vercel.app/"
+    baseURL: "https://bistro-boss-server-e872f67a2-bayzed-ahmeds-projects.vercel.app/",
+    withCredentials: true
 })
 const useAxiosPublic = () => {
     return axiosPublic;
